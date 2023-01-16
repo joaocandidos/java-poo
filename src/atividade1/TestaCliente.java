b76d23eda862875bf0668d4a3945310a853deef7
joaocandidos <120682757+joaocandidos@users.noreply.github.com>
2023-01-16 13:45:20 -0300

    adicionei o exercicio1

---
+++ src/atividade1/TestaCliente.java
@@ -0,0 +1,10 @@
+package atividade1;
+
+public class TestaCliente {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente(01,30,321666,291255,"João");
+        Cliente cliente2 = new Cliente(02,35,321444,123456,"Maria");
+        cliente1.vusializar();
+        cliente2.vusializar();
+    }
+}
